90169efaf8536a10291478f62121f9d12cee1a9a
Mare <mareseestern2@gmail.com>
2020-09-15 15:28:05 +0200

    update

--- ImagePreprocessing/cascade.py
+++ ImagePreprocessing/cascade.py
@@ -20,6 +20,6 @@ biggest = leaf[i_biggest]
 img = cv2.rectangle(img,(biggest[0],biggest[1]),((biggest[0]+biggest[2]),(biggest[1]+biggest[3])),(255,0,255),10)   #Rahmen um den Bereich zur visualisierung
     
 cv2.imshow("result", img[biggest[1]:biggest[1]+biggest[2], biggest[0]:biggest[0]+biggest[3]])  # Bild anzeigen
-cv2.imwrite('result.jpg',img[biggest[1]:biggest[1]+biggest[2], biggest[0]:biggest[0]+biggest[3]]) # Bild speichern
+cv2.imwrite('exampleHaarCascade.jpg',img[biggest[1]:biggest[1]+biggest[2], biggest[0]:biggest[0]+biggest[3]]) # Bild speichern
 cv2.waitKey(0) # clean up
 cv2.destroyAllWindows()
